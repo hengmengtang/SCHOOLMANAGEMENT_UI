79a3954cae2052d3029c6b8dfaa6935efaad3532
hengmengtang <heng.mengtang@gmail.com>
2016-07-31 20:44:16 +0700

    new initialize

---
+++ src/main/java/kshrd/schoolmangement/HrdschoolmangementUiApplication.java
@@ -0,0 +1,12 @@
+package kshrd.schoolmangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrdschoolmangementUiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrdschoolmangementUiApplication.class, args);
+	}
+}
---
+++ src/test/java/kshrd/schoolmangement/HrdschoolmangementUiApplicationTests.java
@@ -0,0 +1,16 @@
+package kshrd.schoolmangement;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HrdschoolmangementUiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
